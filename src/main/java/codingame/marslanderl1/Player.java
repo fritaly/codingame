4f895c610922aeb76321ddeda255bb8b7afc0191
fritaly <francois.ritaly@gmail.com>
2015-09-21 21:53:20 +0200

    Added solution for 'Mars Lander - Level 1'

---
+++ src/main/java/codingame/marslanderl1/Player.java
@@ -0,0 +1,32 @@
+package codingame.marslanderl1;
+
+import java.util.Scanner;
+
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int surfaceN = in.nextInt(); // the number of points used to draw the surface of Mars.
+        for (int i = 0; i < surfaceN; i++) {
+            int landX = in.nextInt(); // X coordinate of a surface point. (0 to 6999)
+            int landY = in.nextInt(); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+        }
+
+        // game loop
+        while (true) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            int hSpeed = in.nextInt(); // the horizontal speed (in m/s), can be negative.
+            int vSpeed = in.nextInt(); // the vertical speed (in m/s), can be negative.
+            int fuel = in.nextInt(); // the quantity of remaining fuel in liters.
+            int rotate = in.nextInt(); // the rotation angle in degrees (-90 to 90).
+            int power = in.nextInt(); // the thrust power (0 to 4).
+            
+            if (vSpeed <= -40) {
+            	System.out.println("0 4");
+            } else {
+            	System.out.println("0 0");
+            }
+        }
+    }
+}
