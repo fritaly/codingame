3db3c47967765b1eb6b52df362bf99bebc42e404
Francois Ritaly <francois.ritaly@gmail.com>
2020-06-23 18:09:05 +0200

    Added solution for 'Brackets Extreme Edition'

---
+++ src/main/groovy/codingame/brackets/Solution.groovy
@@ -0,0 +1,35 @@
+package codingame.brackets
+
+input = new Scanner(System.in);
+
+expression = input.next()
+
+def stack = new Stack()
+def valid = true
+
+for (c in expression) {
+    if ((c == '(') || (c == '[') || (c == '{')) {
+        stack << c
+    } else if (c == ')') {
+        if (stack.empty || (stack.pop() != '(')) {
+            valid = false
+            break
+        }
+    } else if (c == ']') {
+        if (stack.empty || (stack.pop() != '[')) {
+            valid = false
+            break
+        }
+    } else if (c == '}') {
+        if (stack.empty || (stack.pop() != '{')) {
+            valid = false
+            break
+        }
+    }
+}
+
+if (!stack.empty) {
+    valid = false
+}
+
+println valid
