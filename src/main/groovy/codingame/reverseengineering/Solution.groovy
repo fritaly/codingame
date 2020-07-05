05bf80e64ccc644ddd650d081fe5036619790006
Francois Ritaly <francois.ritaly@gmail.com>
2020-07-05 20:51:40 +0200

    Preserve the positions from the previous turn

--- src/main/groovy/codingame/reverseengineering/Solution.groovy
+++ src/main/groovy/codingame/reverseengineering/Solution.groovy
@@ -1,5 +1,19 @@
 package codingame.reverseengineering
 
+class Position {
+    int x, y
+
+    Position(int x, int y) {
+        this.x = x
+        this.y = y
+    }
+
+    @Override
+    String toString() {
+        "(${x}, ${y})"
+    }
+}
+
 void dump(char[][] array) {
     array.each { row ->
         System.err.println("${new String(row)}")
@@ -19,15 +33,23 @@ grid.each { row ->
     Arrays.fill(row, '.' as char)
 }
 
+// Array storing the previous position of each entity
+def previousPositions = null
+
 while (true) {
     firstInput = input.nextLine()
     secondInput = input.nextLine()
     thirdInput = input.nextLine()
     fourthInput = input.nextLine()
 
+    def positions = new Position[5]
+
     for (i = 0; i < entityCount; ++i) {
         def x = input.nextInt()
         def y = input.nextInt()
+        def position = new Position(x, y)
+
+        positions[i] = position
 
         grid[y][x] = "${i+1}".charAt(0)
     }
@@ -37,4 +59,7 @@ while (true) {
     println 'A'
 
     dump(grid)
+
+    // Save the positions for the next turn
+    previousPositions = positions
 }
