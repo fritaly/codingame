c673d45b3ad84f88bc701216b0111af75b124031
Francois Ritaly <francois.ritaly@gmail.com>
2021-02-13 17:48:55 +0100

    Finalized solution for "Code vs Zombies"

--- src/main/dart/code_vs_zombies.dart
+++ src/main/dart/code_vs_zombies.dart
@@ -21,8 +21,6 @@ class Position {
     return sqrt(dx * dx + dy * dy);
   }
 
-  operator +(Vector vector) => Position(x + vector.x, y + vector.y);
-
   @override
   bool operator ==(Object that) {
     if (that is Position) {
@@ -35,27 +33,6 @@ class Position {
   @override
   String toString() {
     return "(${x},${y})";
-  }
-}
-
-// ============== //
-// === Vector === //
-// ============== //
-
-class Vector {
-  final int x, y;
-
-  Vector(this.x, this.y);
-
-  operator *(int n) => Vector(x * n, y * n);
-
-  factory Vector.from(Position start, Position end) {
-    return Vector(end.x - start.x, end.y - start.y);
-  }
-
-  @override
-  String toString() {
-    return "Vector(${x}, ${y})";
   }
 }
 
@@ -78,7 +55,7 @@ abstract class Entity {
 
   /// Returns the estimated time (as a number of turns) to have the target
   /// position in shooting range
-  double estimatedTimeToRange(Position target) => min(0, distanceTo(target) - 2000) / speed;
+  double estimatedTimeToRange(Position target) => max(0, distanceTo(target) - 2000) / speed;
 }
 
 class Player extends Entity {
@@ -119,13 +96,6 @@ class Zombie extends Entity {
 
   Zombie(this.id, Position position, this.nextPosition): super(position, 400);
 
-  /// Returns a vector representing the zombie's movement
-  Vector get vector => Vector.from(position, nextPosition);
-
-  /// Extrapolates the zombie's position after n turns based on its current
-  /// position and movement
-  Position extrapolatePosition(int n) => position + (vector * n);
-
   factory Zombie.read(Stdin stdin) {
     var inputs = stdin.readLineSync().split(' ').map((e) => int.parse(e)).toList();
 
@@ -140,39 +110,37 @@ class Zombie extends Entity {
   }
 }
 
-class Pair implements Comparable<Pair> {
+class Triplet implements Comparable<Triplet> {
   final Human human;
   final Zombie zombie;
+  final Player player;
 
-  Pair(this.human, this.zombie);
+  Triplet(this.human, this.zombie, this.player);
 
-  double get distance => zombie.distanceTo(human.position);
+  /// Returns the distance between the zombie and the human
+  double get zombieDistance => zombie.distanceTo(human.position);
 
   /// Returns the estimated time before the zombie kills the human
-  double get estimatedTime => zombie.estimatedTimeTo(human.position);
+  double get zombieTime => zombie.estimatedTimeTo(human.position);
 
-  /// Returns the entity (that is the human or the zombie) who's the closest to
-  /// the given position
-  Entity closestTo(Position position) {
-    var distanceToHuman = human.distanceTo(position);
-    var distanceToZombie = zombie.distanceTo(position);
+  /// Returns the estimated time before the player can save the human
+  double get playerTime => player.estimatedTimeToRange(human.position);
 
-    return (distanceToZombie < distanceToHuman) ? zombie : human;
-  }
+  /// Tells whether the human can be saved
+  bool get humanCanBeSaved => playerTime < zombieTime;
 
   @override
   String toString() {
-    return "Pair[${distance.toStringAsFixed(0)} - ${human} / ${zombie}]";
+    return "Pair[PT: ${playerTime.toStringAsFixed(1)} - ZT: ${zombieTime.toStringAsFixed(1)} - ${human} / ${zombie}]";
   }
 
   @override
-  int compareTo(Pair other) {
-    return this.distance.compareTo(other.distance);
+  int compareTo(Triplet other) {
+    return this.zombieDistance.compareTo(other.zombieDistance);
   }
 }
 
 void main() {
-
   var round = 1;
 
   while (true) {
@@ -186,103 +154,42 @@ void main() {
 
     var player = Player.read(stdin);
 
-    var humans = <Human>[];
-
-    var humanCount = int.parse(stdin.readLineSync());
+    var humans = <Human>[], humanCount = int.parse(stdin.readLineSync());
 
     for (int i = 0; i < humanCount; i++) {
       humans.add(Human.read(stdin));
     }
 
-    var zombies = <Zombie>[];
-
-    var zombieCount = int.parse(stdin.readLineSync());
+    var zombies = <Zombie>[], zombieCount = int.parse(stdin.readLineSync());
 
     for (int i = 0; i < zombieCount; i++) {
       zombies.add(Zombie.read(stdin));
     }
 
-    // Find the closest zombie to a human
-    var pairs = <Pair>[];
+    var triplets = <Triplet>[];
 
     for (var zombie in zombies) {
       for (var human in humans) {
-        pairs.add(Pair(human, zombie));
+        triplets.add(Triplet(human, zombie, player));
       }
     }
 
-    // Sort the human / zombie pairs by descending distance and distance from
-    // the player
-    pairs.sort((a, b) {
-      if (a.distance == b.distance) {
+    // Sort the triplets by ascending zombie distance (handle the most urgent
+    // situation first)
+    triplets.sort((a, b) {
+      if (a.zombieDistance == b.zombieDistance) {
         return a.zombie.distanceTo(player.position).compareTo(b.zombie.distanceTo(player.position));
       }
 
-      return a.distance.compareTo(b.distance);
+      return a.zombieDistance.compareTo(b.zombieDistance);
     });
 
     trace("Pairs:");
-    trace("${pairs.join('\n')}");
+    trace("${triplets.join('\n')}");
 
-    // Index of the zombie / human pair being considered
-    var index = 0;
-    var moved = false;
+    // Only consider the situations where the human can still be saved
+    triplets = triplets.where((pair) => pair.humanCanBeSaved).toList();
 
-    while ((index < pairs.length) && !moved) {
-      var selection = pairs[index];
-
-      var zombie = selection.zombie;
-      var human = selection.human;
-
-      trace("Processing pair ${human} / ${zombie} ...");
-      trace("Distance human/zombie: ${selection.distance} (ETA: ${selection.estimatedTime} turns)");
-      trace('Distance zombie/player: ${zombie.distanceTo(player.position)}');
-      trace('Distance zombie/player (minus shoot range): ${zombie.distanceTo(player.position) - 2000}');
-
-      // Time before the zombie kills the human ?
-      var zombieETA = selection.estimatedTime;
-
-      // Can we reach the zombie before he kills the human ?
-      var turns = 1;
-
-      while (turns <= zombieETA) {
-        trace("After ${turns} turns ...");
-
-        var futurePosition = zombie.extrapolatePosition(turns);
-
-        trace('The zombie position will be ${futurePosition}');
-
-        // Can the player reach this position in N turns ?
-        var playerETA = player.estimatedTimeToRange(futurePosition);
-
-        trace("Player ETA: ${playerETA}");
-
-        if (playerETA <= turns) {
-          // Yes, the player can reach the position before the zombie gets the
-          // human
-          trace("The player can save the human in ${turns} turn(s)");
-
-          print('${futurePosition.x} ${futurePosition.y}');
-          moved = true;
-          break;
-        }
-
-        trace("Checking next turn ...");
-
-        // Check if we can kill the zombie with one more turn
-        turns++;
-      }
-
-      if (!moved) {
-        // We cannot save this human in time, check the next pair
-        trace("No solution found, checking next pair ...");
-
-        index++;
-      }
-    }
-
-    if (!moved) {
-      print("${player.x} ${player.y}");
-    }
+    print("${triplets[0].human.x} ${triplets[0].human.y}");
   }
 }
