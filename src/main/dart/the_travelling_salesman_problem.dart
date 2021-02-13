7fbfc09ca827a4e6aabbe2e099439599007a5626
Francois Ritaly <francois.ritaly@gmail.com>
2021-02-13 13:44:05 +0100

    Added solution for "The travelling salesman problem"

---
+++ src/main/dart/the_travelling_salesman_problem.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+
+import 'dart:math';
+
+void trace(String message) {
+  stderr.writeln("${message}");
+}
+
+class Coordinates {
+  final int x, y;
+
+  const Coordinates(this.x, this.y);
+
+  double distance(Coordinates other) {
+    assert (other != null);
+
+    var dx = this.x - other.x;
+    var dy = this.y - other.y;
+
+    return sqrt(dx * dx + dy * dy);
+  }
+
+  @override
+  String toString() {
+    return "(${x},${y})";
+  }
+}
+
+List<Coordinates> findSolution(List<Coordinates> steps, List<Coordinates> remainder) {
+  if (remainder.isEmpty) {
+    return steps;
+  }
+
+  var current = steps.last;
+  var next = null, minDistance = double.maxFinite;
+
+  for (var coordinates in remainder) {
+    var distance = current.distance(coordinates);
+
+    if ((next == null) || (distance < minDistance)) {
+      minDistance = distance;
+      next = coordinates;
+    }
+  }
+
+  var remainder2 = List<Coordinates>.from(remainder);
+  remainder2.remove(next);
+
+  return findSolution(steps + [ next ], remainder2);
+}
+
+void main() {
+  var N = int.parse(stdin.readLineSync());
+
+  trace("${N}");
+
+  var allCoordinates = <Coordinates>[];
+
+  for (var i = 0; i < N; i++) {
+    var inputs = stdin.readLineSync().split(' ');
+
+    allCoordinates.add(Coordinates(int.parse(inputs[0]), int.parse(inputs[1])));
+  }
+
+  trace("${allCoordinates.map((e) => "${e.x} ${e.y}").join('\n')}");
+
+  // Close the loop by adding the start position at the end of the trip
+  var steps = findSolution([ (allCoordinates[0]) ], allCoordinates.sublist(1)) + [ (allCoordinates[0]) ];
+
+  var totalDistance = 0.0;
+
+  for (var i = 1; i < steps.length; i++) {
+    totalDistance += steps[i - 1].distance(steps[i]);
+  }
+
+  print(totalDistance.round());
+}
