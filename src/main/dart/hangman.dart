62121c38cec936210236edd72f49ba57aba84f4e
Francois Ritaly <francois.ritaly@gmail.com>
2021-02-20 11:06:16 +0100

    Added solution for "Hangman"

---
+++ src/main/dart/hangman.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+
+void trace(String message) {
+  stderr.writeln("${message}");
+}
+
+String draw(int errors) {
+  switch (errors) {
+    case 0:
+      return '+--+\n|\n|\n|\\';
+    case 1:
+      return '+--+\n|  o\n|\n|\\';
+    case 2:
+      return '+--+\n|  o\n|  |\n|\\';
+    case 3:
+      return '+--+\n|  o\n| /|\n|\\';
+    case 4:
+      return '+--+\n|  o\n| /|\\\n|\\';
+    case 5:
+      return '+--+\n|  o\n| /|\\\n|\\/';
+    case 6:
+      return '+--+\n|  o\n| /|\\\n|\\/ \\';
+    default:
+      throw "Unexpected value: ${errors}";
+  }
+}
+
+bool equalsIgnoreCase(String s1, String s2) => s1.toLowerCase() == s2.toLowerCase();
+
+class Hangman {
+  final String word;
+  final List<String> characters = [];
+  int errors = 0;
+
+  Hangman(this.word) {
+    this.characters.addAll(word.split('').map((c) => (c != ' ') ? '_' : ' '));
+  }
+
+  bool get success => characters.every((c) => c != '_');
+  bool get failure => characters.any((c) => c == '_') || (errors >= 6);
+
+  void guess(String letter) {
+    var found = false;
+    for (var n = 0; n < word.length; n++) {
+      if (equalsIgnoreCase(word[n], letter) && (characters[n] == '_')) {
+        characters[n] = word[n];
+        found = true;
+      }
+    }
+
+    if (!found) {
+      errors++;
+    }
+  }
+
+  @override
+  String toString() => "Hangman[${characters.join()}]";
+}
+
+void main() {
+  var word = stdin.readLineSync();
+  var chars = stdin.readLineSync().split(' ').toList();
+
+  trace("${word}");
+  trace("${chars.join(' ')}");
+
+  var hangman = Hangman(word);
+
+  for (var letter in chars) {
+    hangman.guess(letter);
+
+    trace("${hangman} - ${hangman.success} - ${hangman.errors} error(s)");
+  }
+
+  print(draw(hangman.errors));
+  print(hangman.characters.join());
+}
