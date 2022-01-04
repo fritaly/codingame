4e9c7e203aee1fe2493b6201a637db33c43b3d38
Francois Ritaly <francois.ritaly@gmail.com>
2022-01-04 17:06:26 +0100

    Updated the license headers

--- codingame-java/src/main/java/codingame/powerofthor/Player.java
+++ codingame-java/src/main/java/codingame/powerofthor/Player.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2021, Francois Ritaly
+ * Copyright 2015-2022, Francois Ritaly
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
